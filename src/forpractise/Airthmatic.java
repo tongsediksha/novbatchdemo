3acbace770ad09a6c4f35ecfa065176b75c45aed
switi <switi@switi-pc>
2022-11-17 10:12:38 +0530

    [novtodo]

---
+++ src/forpractise/Airthmatic.java
@@ -0,0 +1,27 @@
+package forpractise;
+
+public class Airthmatic {
+	int a= 23;
+	float b=23.24f;
+	double u=43.56677;
+	long l= 453678;
+	public  void add(int c,int n) {		
+		float m= c+n;
+		System.out.println(m);
+	}
+	public void mul( double d, int a){
+		double s= a*d;
+		
+	System.out.println(s); 
+		
+	}
+
+	public static void main(String[] args) {
+		Airthmatic z=new Airthmatic();		
+		z.add(12,56);		
+		z. mul(32.23, 056);
+	}
+}
+
+
+
---
+++ src/forpractise/Statickeywords.java
@@ -0,0 +1,29 @@
+package forpractise;
+public class Statickeywords {
+		int a=12;
+		float b=23.4f;
+		Integer k=23;
+		Float c=12.14f;
+		Character ch='d';
+
+		public void m1(){
+
+		Integer s= new Integer(234);
+		Float t= new Float(22.2);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(k);
+		System.out.println(ch);
+		System.out.println(s);
+		System.out.println(t);
+		}
+		public static void main(String[]args){
+			Statickeywords t=new Statickeywords();
+		t.m1();
+		}
+		
+			
+
+		}
+
+
